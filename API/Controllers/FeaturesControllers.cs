1da84fa7791cd8c55307a0e56f25635989291a07
bensiton69 <bensiton69@gmail.com>
2022-01-08 10:29:55 +0200

    Start the Front-end

---
+++ API/Controllers/FeaturesControllers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using API.Enums;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeaturesController : ControllerBase
+    {
+        [HttpGet("Genres")]
+        public IEnumerable<string> GetStatuses()
+        {
+            return Enum.GetValues(typeof(eGenre))
+                .Cast<eGenre>()
+                .Select(v => v.ToString());
+        }
+
+        [HttpGet("MovieTypes")]
+        public IEnumerable<string> GetMovieTypes()
+        {
+            return Enum.GetValues(typeof(eMovieType))
+                .Cast<eMovieType>()
+                .Select(v => v.ToString());
+        }
+
+        [HttpGet("SeatTypes")]
+        public IEnumerable<string> GetSeatType()
+        {
+            return Enum.GetValues(typeof(eMovieType))
+                .Cast<eMovieType>()
+                .Select(v => v.ToString());
+        }
+    }
+}
